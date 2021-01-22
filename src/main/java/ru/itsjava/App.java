a5df40e9e32e956605945eca8aeebc921cf0ad94
Pavel Semenenko <pavelsemenenko.spb@gmail.com>
2021-01-22 21:59:24 +0300

    added registration

--- src/main/java/ru/itsjava/App.java
+++ src/main/java/ru/itsjava/App.java
@@ -17,35 +17,61 @@ public class App {
         System.out.println("I'm connected");
 
         BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
-
         PrintWriter socketWriter = new PrintWriter(socket.getOutputStream());
-
         while (true) {
-            System.out.println("Введите логин: ");
-            String login = consoleReader.readLine();
 
-            System.out.println("Введите пароль: ");
-            String password = consoleReader.readLine();
-
-            socketWriter.println("!auth!" + login + ":" + password);
+            System.out.println("Введите 1 для регистрации, введите 2 для авторизации");
+            String numMenu = consoleReader.readLine();
+            socketWriter.println(numMenu);
             socketWriter.flush();
-            BufferedReader serverReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            if (numMenu.equals("1")) {
+                while (true) {
+                    System.out.println("Введите логин: ");
+                    String login = consoleReader.readLine();
 
-            String serverMessage = serverReader.readLine();
-            System.out.println(serverMessage);
-            if (serverMessage.equals("Вы успешно авторизовались")) {
+                    System.out.println("Введите пароль: ");
+                    String password = consoleReader.readLine();
+
+                    socketWriter.println("!auth!" + login + ":" + password);
+                    socketWriter.flush();
+                    BufferedReader serverReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                    String serverMessage = serverReader.readLine();
+                    System.out.println(serverMessage);
+                    if (serverMessage.equals("Вы успешно авторизовались")) {
+                        break;
+                    }
+                }
                 break;
             }
-        }
-       new Thread(new ServerRunnable(socket)).start();
+            if (numMenu.equals("2")) {
 
-        while (true) {
-            socketWriter.println(consoleReader.readLine());
-            socketWriter.flush();
+                System.out.println("Введите новый логин: ");
+                String login = consoleReader.readLine();
+                socketWriter.println(login);
+                socketWriter.flush();
+
+                System.out.println("Введите новый пароль: ");
+                String password = consoleReader.readLine();
+                socketWriter.println(password);
+                socketWriter.flush();
+
+                System.out.println("Регистрация успешна");
+
+
+            }
+
         }
+            new Thread(new ServerRunnable(socket)).start();
+
+            while (true) {
+                socketWriter.println(consoleReader.readLine());
+                socketWriter.flush();
+            }
 //        Thread.sleep(5000);
 //        socketWriter.println("Privet");
 //        socketWriter.flush();
 
+        }
     }
-}
+
