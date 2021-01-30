2774426d5eee2ee0e92881fbca70177582777f27
Pavel Semenenko <pavelsemenenko.spb@gmail.com>
2021-01-30 19:14:33 +0300

    delete unnecessary annotation

--- src/main/java/ru/itsjava/App.java
+++ src/main/java/ru/itsjava/App.java
@@ -1,12 +1,7 @@
 package ru.itsjava;
-
-import lombok.SneakyThrows;
 import ru.itsjava.services.ClientImpl;
 
 public class App {
-
-
-    @SneakyThrows
     public static void main(String[] args) {
         new ClientImpl().start();
         }
